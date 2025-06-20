1577472401f8e9f72b3b7afad13c6124f00d8579
Alan Veliz-Cuba <alanavc@users.noreply.github.com>
2025-06-20 17:18:03 -0400

    Update script_toy.m
    updating old script to work with current code

--- script_toy.m
+++ script_toy.m
@@ -15,7 +15,8 @@ generate_wiring_diagrams(file_for_disc_data,file_for_wiring_diagrams)
 
 %% select wiring diagram
 addpath('select_WD')
-W=select_best_wiring_diagram(file_for_wiring_diagrams);
+threshold=1/2
+W=select_best_wiring_diagram(file_for_wiring_diagrams,threshold);
 
 %% create model
 file_for_model='model0.mat';
